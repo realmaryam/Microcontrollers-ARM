f883eb0b013c3421f73496a205d5252ada2b3e4f
Maryam Barazande <84562248+realmaryam@users.noreply.github.com>
2022-06-22 21:45:13 +0430

    Add files via upload

---
+++ DC motor/LCD/LCD16x2_cfg.c
@@ -0,0 +1,26 @@
+  
+/*
+ * File: LCD16x2_cfg.c
+ * Driver Name: [[ LCD16x2 Display (GPIO 4-Bit Mode) ]]
+ * SW Layer:   ECUAL
+ * Created on: Jun 28, 2020
+ * Author:     Khaled Magdy
+ * -------------------------------------------
+ * For More Information, Tutorials, etc.
+ * Visit Website: www.DeepBlueMbedded.com
+ *
+ */
+
+#include "LCD16x2.h"
+
+const LCD16x2_CfgType LCD16x2_CfgParam =
+{
+	GPIOB,
+	GPIO_PIN_5,
+	GPIO_PIN_6,
+	GPIO_PIN_7,
+	GPIO_PIN_8,
+	GPIO_PIN_3,
+	GPIO_PIN_4,
+	20
+};
